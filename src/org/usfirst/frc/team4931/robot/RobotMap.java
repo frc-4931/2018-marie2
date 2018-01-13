a611fef333c4902d507d6df4a278478963cf9bb3
Mary <meep000000@yahoo.com>
2018-01-12 20:08:52 -0600

    Modified Drivetrain class: Defining motors Added drive method

--- src/org/usfirst/frc/team4931/robot/RobotMap.java
+++ src/org/usfirst/frc/team4931/robot/RobotMap.java
@@ -16,8 +16,11 @@ package org.usfirst.frc.team4931.robot;
 public class RobotMap {
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
+	public static int leftFrontMotorPort = 1;
+	public static int leftBackMotorPort = 2;
+	public static int rightFrontMotorPort = 3;
+	public static int rightBackMotorPort = 4;
+
 
 	// If you are using multiple modules, make sure to define both the port
 	// number and the module. For example you with a rangefinder:
--- src/org/usfirst/frc/team4931/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team4931/robot/subsystems/Drivetrain.java
@@ -1,14 +1,37 @@
 package org.usfirst.frc.team4931.robot.subsystems;
 
+import org.usfirst.frc.team4931.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;;
 
 public class Drivetrain extends Subsystem {
-
+	private static WPI_TalonSRX leftFrontMotor;
+	private static WPI_TalonSRX leftBackMotor;
+	private static WPI_TalonSRX rightFrontMotor;
+	private static WPI_TalonSRX rightBackMotor;
+	private static SpeedControllerGroup leftSide;
+	private static SpeedControllerGroup rightSide;
+	private static DifferentialDrive drivetrain;
+	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 		
+		leftFrontMotor = new WPI_TalonSRX(RobotMap.leftFrontMotorPort);
+		leftBackMotor = new WPI_TalonSRX(RobotMap.leftBackMotorPort);
+		leftSide = new SpeedControllerGroup(leftFrontMotor, leftBackMotor);
+		rightFrontMotor = new WPI_TalonSRX(RobotMap.rightFrontMotorPort);
+		rightBackMotor = new WPI_TalonSRX(RobotMap.rightBackMotorPort);
+		rightSide = new SpeedControllerGroup(rightFrontMotor,rightBackMotor);
+		drivetrain = new DifferentialDrive(leftSide, rightSide);
 	}
-
-
+	
+	public void drive(double speed, double rotation) {
+		drivetrain.arcadeDrive(speed, rotation);
+	}
+	
 }
