1479999fad04ae472419ca5ca16b9dca8137f8c9
DJWICK2000 <dj_bakugan@Charter.net>
2018-01-16 18:54:43 -0600

    Added functionality to the Lift class

--- src/org/usfirst/frc/team4931/robot/subsystems/Lift.java
+++ src/org/usfirst/frc/team4931/robot/subsystems/Lift.java
@@ -1,8 +1,12 @@
 package org.usfirst.frc.team4931.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lift extends Subsystem {
+	private WPI_TalonSRX liftMotor;
+	private static final double LIFT_SPEED = 1;
 
 	@Override
 	protected void initDefaultCommand() {
@@ -11,17 +15,17 @@ public class Lift extends Subsystem {
 	}
 	
 	/**
-	 * Method to get lifter to rise up
+	 * Lifting mechanism will rise up full speed
 	 */
 	public void liftUp() {
-		
+		liftMotor.set(LIFT_SPEED);
 	}
 	
 	/**
-	 * Method to get lifter to fall down
+	 * Lifting mechanism will descend full speed
 	 */
 	public void liftDown() {
-		
+		liftMotor.set(-LIFT_SPEED);
 	}
 
 }
