770f097f1dd4d9fbd5d535310f2578a548c51344
DJWICK2000 <dj_bakugan@Charter.net>
2018-01-16 19:33:22 -0600

    Redid functionality to the lift class, where a potentiometer was used.

--- src/org/usfirst/frc/team4931/robot/subsystems/Lift.java
+++ src/org/usfirst/frc/team4931/robot/subsystems/Lift.java
@@ -1,31 +1,48 @@
 package org.usfirst.frc.team4931.robot.subsystems;
 
+import org.usfirst.frc.team4931.robot.RobotMap;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Lift extends Subsystem {
+public class Lift extends PIDSubsystem {
 	private WPI_TalonSRX liftMotor;
-	private static final double LIFT_SPEED = 1;
+	private AnalogPotentiometer potentiometer;
+	private static final double kP_real = 4;
+	private static final double kI_real = 0.07;
 
+	
+	
+	public Lift() {
+		super(kP_real, kI_real, 0);
+		setAbsoluteTolerance(0.005);
+		liftMotor = new WPI_TalonSRX(RobotMap.liftMotorPort);
+		potentiometer = new AnalogPotentiometer(2, -2.0 / 5);
+		addChild("Motor", liftMotor);
+		addChild("Potentiometer", potentiometer);
+	}
+	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 		
 	}
 	
-	/**
-	 * Lifting mechanism will rise up full speed
-	 */
-	public void liftUp() {
-		liftMotor.set(LIFT_SPEED);
+	public void log() {
+		SmartDashboard.putData("Lift Potentiometer", (AnalogPotentiometer) potentiometer);
 	}
 	
-	/**
-	 * Lifting mechanism will descend full speed
-	 */
-	public void liftDown() {
-		liftMotor.set(-LIFT_SPEED);
+	@Override
+	protected double returnPIDInput() {
+		return potentiometer.get();
 	}
-
+	
+	@Override
+	protected void usePIDOutput(double power) {
+		liftMotor.set(power);
+	}
 }
