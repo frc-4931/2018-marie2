d94a70a2d510d1c094e5e485bde3d38da4a81513
DJWICK2000 <dj_bakugan@Charter.net>
2018-01-27 16:47:07 -0600

    Finished basic outline of FieldAnalyzer class

--- src/org/usfirst/frc/team4931/robot/commands/TurnToAngle.java
+++ src/org/usfirst/frc/team4931/robot/commands/TurnToAngle.java
@@ -6,30 +6,30 @@ import edu.wpi.first.wpilibj.command.Command;
 public class TurnToAngle extends Command {
   private double speed;
   private double angle;
-  
+
   public TurnToAngle(double speed, double angle) {
     requires(Robot.drivetrain);
-    
+
     this.speed = speed;
     this.angle = angle;
   }
-  
+
   @Override
   protected void initialize() {
     Robot.drivetrain.gyroReset();
     Robot.drivetrain.driveTank(speed, -speed);
   }
-  
+
   @Override
   protected boolean isFinished() {
     // TODO Auto-generated method stub
-    if (Math.abs(Robot.drivetrain.gyroReadAngle()) >= Math.abs(angle)) {
+    if (Math.abs(Robot.drivetrain.gyroReadYawAngle()) >= Math.abs(angle)) {
       return true;
     } else {
       return false;
     }
   }
-  
+
   @Override
   protected void end() {
     Robot.drivetrain.driveTank(0, -0);
--- src/org/usfirst/frc/team4931/robot/field/FieldAnalyzer.java
+++ src/org/usfirst/frc/team4931/robot/field/FieldAnalyzer.java
@@ -1,14 +1,24 @@
 package org.usfirst.frc.team4931.robot.field;
 
 import java.util.EnumMap;
+import java.util.List;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Trajectory.FitMethod;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.TankModifier;
 
 public class FieldAnalyzer {
 
   private char fieldPos[]; // position of switches and scale
-  private EnumMap<Strategy, String> strategyOptions = new EnumMap<>(Strategy.class);
+  private EnumMap<Strategy, TankModifier> strategyOptions = new EnumMap<>(Strategy.class);
   private StartingPos robotStartingPos; // position of the robot before auto
   private char strategyPick[]; // selected strategy by the drive team
+  private double dt = 0.05; // time between each different straight path on the curve
+  private double maxSpeed = 4.0;
+  private double maxAcceleration = 3.0;
+  private double maxJerk = 60.0;
 
   public void setFieldPosition(char[] fieldPosition) {
     fieldPos = fieldPosition; // Removing bugs created more bugs
@@ -18,10 +28,14 @@ public class FieldAnalyzer {
     strategyPick = SmartDashboard.getString("Strategy Field", "nnnnn").toLowerCase().toCharArray();
     robotStartingPos = StartingPos
         .valueOf(SmartDashboard.getString("Position Selection", StartingPos.LEFT.name()));
+    Trajectory.Config config = new Trajectory.Config(FitMethod.HERMITE_CUBIC,
+        Trajectory.Config.SAMPLES_FAST, dt, maxSpeed, maxAcceleration, maxJerk);
     for (Strategy s : Strategy.values()) {
       if (strategyPick[s.ordinal()] == 'y') {
-        int[][] point = Waypoints.WAYPOINTS.get(robotStartingPos).get(s);
-        strategyOptions.put(s, "test");
+        List<Waypoint> point = Waypoints.WAYPOINTS.get(robotStartingPos).get(s);
+        TankModifier tankModifier =
+            new TankModifier(Pathfinder.generate(point.toArray(new Waypoint[] {}), config));
+        strategyOptions.put(s, tankModifier);
       }
     }
   }
