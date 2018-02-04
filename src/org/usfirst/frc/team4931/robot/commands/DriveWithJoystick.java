0e268cf93345122fdf8b43b5bee6b2a7e6e930de
DJWICK2000 <dj_bakugan@Charter.net>
2018-02-04 15:12:05 -0600

    added comments to the DriveWithJoystick and LiftWithJoystick classes

--- src/org/usfirst/frc/team4931/robot/commands/DriveWithJoystick.java
+++ src/org/usfirst/frc/team4931/robot/commands/DriveWithJoystick.java
@@ -4,23 +4,39 @@ import org.usfirst.frc.team4931.robot.Robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
+/**
+ * @author dj wickman
+ *
+ * Allows manual control of the drivetrain with the joystick. 
+ */
 public class DriveWithJoystick extends Command {
   
   public DriveWithJoystick() {
     requires(Robot.drivetrain);
   }
   
+  /**
+   * Feeds the input of the Joystick into the driveArchade() method in
+   * the Drivetrain class.
+   */
   @Override
   protected void execute() {
     Joystick controller = Robot.operatorInput.getDriverController();
     Robot.drivetrain.driveArcade(controller.getY(), controller.getZ(), controller.getThrottle());
   }
   
+  /**
+   * Keeps the command from stopping on its own.
+   */
   @Override
   protected boolean isFinished() {
     return false;
   }
   
+  /**
+   * Ends the manual motion of the drivetrain. Will be used when the input
+   * from the joystick ends.
+   */
   @Override
   protected void end() {
     Robot.drivetrain.driveArcade(0, 0, 0);
