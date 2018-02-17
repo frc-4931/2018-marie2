55450c375af8358f79cc92c1face2f2ec112d8c9
James Crane <james@cranesnest.us>
2018-02-17 10:17:09 -0600

    Added comment for the math to convert Pulses/100ms to M/s Added max speed print in Drivetrain's log

--- src/org/usfirst/frc/team4931/robot/Robot.java
+++ src/org/usfirst/frc/team4931/robot/Robot.java
@@ -26,7 +26,6 @@ import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
