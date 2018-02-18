ae286ae8cd16f14530c7a63b9cb0a636ac2b0f69
James Crane <james@cranesnest.us>
2018-02-18 15:37:55 -0600

    Fixed GoToDistance to actually work

--- src/org/usfirst/frc/team4931/robot/commands/GoToDistance.java
+++ src/org/usfirst/frc/team4931/robot/commands/GoToDistance.java
@@ -1,16 +1,36 @@
 package org.usfirst.frc.team4931.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team4931.robot.Robot;
 
 public class GoToDistance extends Command {
+  private double speed;
+  private double distance;
 
-  //FIXME
   public GoToDistance(double speed, double distance) {
+    requires(Robot.drivetrain);
+
+    this.speed = speed;
+    this.distance = distance / (Math.PI*6) * 480;
+
     System.out.println("Distance: " + distance);
   }
 
   @Override
+  protected void initialize() {
+    Robot.drivetrain.gyroReset();
+    Robot.drivetrain.resetLeftEncoder();
+    Robot.drivetrain.driveTank(speed, speed);
+  }
+
+  @Override
+  protected void execute() {
+    double correction = Robot.drivetrain.gyroReadYawAngle() / 50;
+    Robot.drivetrain.driveTank(speed - correction, speed + correction);
+  }
+
+  @Override
   protected boolean isFinished() {
-    return false;
+    return Robot.drivetrain.getLeftEncoder() >= distance;
   }
 }
