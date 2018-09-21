247f45f3e357dc55163ee6148da1cf34ee5ade29
Damian Kocher <damian@localhost.localdomain>
2018-09-20 20:05:19 -0500

    Changed Operator input

--- src/org/usfirst/frc/team4931/robot/OperatorInput.java
+++ src/org/usfirst/frc/team4931/robot/OperatorInput.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team4931.robot;
 
+import org.usfirst.frc.team4931.robot.commands.ChangeGrabberState;
 import org.usfirst.frc.team4931.robot.commands.ShiftGear;
 import org.usfirst.frc.team4931.robot.enums.Gear;
+import org.usfirst.frc.team4931.robot.enums.GrabberState;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
@@ -13,23 +15,22 @@ public class OperatorInput {
 	private Joystick joystick;
 
 	public OperatorInput() {
-		joystick = makeJoystick();
-	}
+		joystick = new Joystick(RobotMap.JOYSTICK.getValue());
 
-	private Joystick makeJoystick() {
-		Joystick joystick = new Joystick(RobotMap.JOYSTICK.getValue());
-
+		/* Grabber */
 		Button grabberOpen = new JoystickButton(joystick, RobotMap.GRABBER_OPEN.getValue());
+		grabberOpen.whenActive(new ChangeGrabberState(GrabberState.OPENED));
+		
 		Button grabberClose = new JoystickButton(joystick, RobotMap.GRABBER_CLOSE.getValue());
-
+		grabberClose.whenActive(new ChangeGrabberState(GrabberState.CLOSED));
+		
+		
 		/* Shifting */
 		Button shiftToLowGear = new JoystickButton(joystick, RobotMap.SHIFT_TO_LOW_GEAR.getValue());
 		shiftToLowGear.whenPressed(new ShiftGear(Gear.LOW));
 
 		Button shiftToHighGear = new JoystickButton(joystick, RobotMap.SHIFT_TO_HIGH_GEAR.getValue());
 		shiftToHighGear.whenPressed(new ShiftGear(Gear.HIGH));
-		
-		return joystick;
 	}
 
 	public Joystick getJoystick() {
